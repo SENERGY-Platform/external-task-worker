f4401e09f7de3d4e118431e3b0c213001c2a1bed
Ingo <roessner@infai.org>
2019-09-05 15:38:04 +0200

    update model with concept characteristics field as id

--- lib/marshaller/casting/base/repo.go
+++ lib/marshaller/casting/base/repo.go
@@ -26,7 +26,7 @@ func (this *ConceptRepoType) Register(concept model.Concept, characteristics []m
 	this.mux.Lock()
 	defer this.mux.Unlock()
 	for _, characteristic := range characteristics {
-		concept.Characteristics = append(concept.Characteristics, characteristic)
+		concept.CharacteristicIds = append(concept.CharacteristicIds, characteristic.Id)
 		this.characteristics[characteristic.Id] = characteristic
 		this.conceptByCharacteristic[characteristic.Id] = concept
 		for _, descendent := range getCharacteristicDescendents(characteristic) {
