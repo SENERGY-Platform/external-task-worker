a62e9cff6d33e09c9d0470f8e7e30e0bd739b77b
Ingo <roessner@infai.org>
2019-11-28 16:42:35 +0100

    add debug messages

--- lib/protocol.go
+++ lib/protocol.go
@@ -31,6 +31,9 @@ import (
 func (this *worker) CreateProtocolMessage(command messages.Command, task messages.CamundaExternalTask) (topic string, message string, err error) {
 	value, err := this.createMessageForProtocolHandler(command, task)
 	if err != nil {
+		if this.config.Debug {
+			log.Println("DEBUG:", task.TenantId)
+		}
 		log.Println("ERROR: on CreateProtocolMessage createMessageForProtocolHandler(): ", err)
 		err = errors.New("internal format error (inconsistent data?) (time: " + util.TimeNow().String() + ")")
 		return
@@ -40,14 +43,11 @@ func (this *worker) CreateProtocolMessage(command messages.Command, task message
 	return topic, string(msg), err
 }
 
-//TODO: replace with keycloak
-const adminjwt = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzaUtabW9aUHpsMmRtQnBJdS1vSkY4ZVVUZHh4OUFIckVOcG5CcHM5SjYwIn0.eyJqdGkiOiJiOGUyNGZkNy1jNjJlLTRhNWQtOTQ4ZC1mZGI2ZWVkM2JmYzYiLCJleHAiOjE1MzA1MzIwMzIsIm5iZiI6MCwiaWF0IjoxNTMwNTI4NDMyLCJpc3MiOiJodHRwczovL2F1dGguc2VwbC5pbmZhaS5vcmcvYXV0aC9yZWFsbXMvbWFzdGVyIiwiYXVkIjoiZnJvbnRlbmQiLCJzdWIiOiJkZDY5ZWEwZC1mNTUzLTQzMzYtODBmMy03ZjQ1NjdmODVjN2IiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJmcm9udGVuZCIsIm5vbmNlIjoiMjJlMGVjZjgtZjhhMS00NDQ1LWFmMjctNGQ1M2JmNWQxOGI5IiwiYXV0aF90aW1lIjoxNTMwNTI4NDIzLCJzZXNzaW9uX3N0YXRlIjoiMWQ3NWE5ODQtNzM1OS00MWJlLTgxYjktNzMyZDgyNzRjMjNlIiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJjcmVhdGUtcmVhbG0iLCJhZG1pbiIsImRldmVsb3BlciIsInVtYV9hdXRob3JpemF0aW9uIiwidXNlciJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im1hc3Rlci1yZWFsbSI6eyJyb2xlcyI6WyJ2aWV3LWlkZW50aXR5LXByb3ZpZGVycyIsInZpZXctcmVhbG0iLCJtYW5hZ2UtaWRlbnRpdHktcHJvdmlkZXJzIiwiaW1wZXJzb25hdGlvbiIsImNyZWF0ZS1jbGllbnQiLCJtYW5hZ2UtdXNlcnMiLCJxdWVyeS1yZWFsbXMiLCJ2aWV3LWF1dGhvcml6YXRpb24iLCJxdWVyeS1jbGllbnRzIiwicXVlcnktdXNlcnMiLCJtYW5hZ2UtZXZlbnRzIiwibWFuYWdlLXJlYWxtIiwidmlldy1ldmVudHMiLCJ2aWV3LXVzZXJzIiwidmlldy1jbGllbnRzIiwibWFuYWdlLWF1dGhvcml6YXRpb24iLCJtYW5hZ2UtY2xpZW50cyIsInF1ZXJ5LWdyb3VwcyJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwicm9sZXMiOlsidW1hX2F1dGhvcml6YXRpb24iLCJhZG1pbiIsImNyZWF0ZS1yZWFsbSIsImRldmVsb3BlciIsInVzZXIiLCJvZmZsaW5lX2FjY2VzcyJdLCJuYW1lIjoiZGYgZGZmZmYiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXBsIiwiZ2l2ZW5fbmFtZSI6ImRmIiwiZmFtaWx5X25hbWUiOiJkZmZmZiIsImVtYWlsIjoic2VwbEBzZXBsLmRlIn0.eOwKV7vwRrWr8GlfCPFSq5WwR_p-_rSJURXCV1K7ClBY5jqKQkCsRL2V4YhkP1uS6ECeSxF7NNOLmElVLeFyAkvgSNOUkiuIWQpMTakNKynyRfH0SrdnPSTwK2V1s1i4VjoYdyZWXKNjeT2tUUX9eCyI5qOf_Dzcai5FhGCSUeKpV0ScUj5lKrn56aamlW9IdmbFJ4VwpQg2Y843Vc0TqpjK9n_uKwuRcQd9jkKHkbwWQ-wyJEbFWXHjQ6LnM84H0CQ2fgBqPPfpQDKjGSUNaCS-jtBcbsBAWQSICwol95BuOAqVFMucx56Wm-OyQOuoQ1jaLt2t-Uxtr-C9wKJWHQ"
-
 func (this *worker) createMessageForProtocolHandler(command messages.Command, task messages.CamundaExternalTask) (result messages.ProtocolMsg, err error) {
 	device := command.Device
 	service := command.Service
 	protocol := command.Protocol
-	token := devicerepository.Impersonate(adminjwt)
+	token := devicerepository.Impersonate("")
 	if device == nil || service == nil || protocol == nil {
 		token, err = this.repository.GetToken(task.TenantId)
 		if err != nil {
@@ -57,6 +57,7 @@ func (this *worker) createMessageForProtocolHandler(command messages.Command, ta
 	if device == nil {
 		temp, err := this.repository.GetDevice(token, command.DeviceId)
 		if err != nil {
+			log.Println("ERROR: unable to load device", command.DeviceId, task.TenantId, token)
 			return result, err
 		}
 		device = &temp
@@ -64,6 +65,7 @@ func (this *worker) createMessageForProtocolHandler(command messages.Command, ta
 	if service == nil {
 		temp, err := this.repository.GetService(token, *device, command.ServiceId)
 		if err != nil {
+			log.Println("ERROR: unable to load device", command.ServiceId, task.TenantId, token)
 			return result, err
 		}
 		service = &temp
@@ -71,6 +73,7 @@ func (this *worker) createMessageForProtocolHandler(command messages.Command, ta
 	if protocol == nil {
 		temp, err := this.repository.GetProtocol(token, command.ProtocolId)
 		if err != nil {
+			log.Println("ERROR: unable to load device", command.ProtocolId, task.TenantId, token)
 			return result, err
 		}
 		protocol = &temp
