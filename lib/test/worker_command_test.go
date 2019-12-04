8919ebfdea3035782f6e5d830f791019e6dc7705
Ingo <roessner@infai.org>
2019-12-04 10:59:14 +0100

    add on/power command tests

--- lib/test/worker_command_test.go
+++ lib/test/worker_command_test.go
@@ -182,3 +182,96 @@ func ExampleWorkerCommand() {
 	//{"request":{"input":{"body":"{\"level\":\"#ff00ff\"}"}},"response":{"output":null},"task_info":{"worker_id":"workerid","task_id":"4","process_instance_id":"","process_definition_id":"","completion_strategy":"optimistic","time":"-62135596800"},"metadata":{"device":{"id":"device_1","local_id":"d1u","name":"d1","device_type_id":"dt1"},"service":{"id":"service_1","local_id":"s1u","name":"s1","description":"","aspects":null,"protocol_id":"p1","inputs":[{"id":"metrics","content_variable":{"id":"metrics","name":"metrics","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"level","name":"level","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_hex","value":null,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},"serialization":"json","protocol_segment_id":"ms1"}],"outputs":null,"functions":null,"rdf_type":""},"protocol":{"id":"p1","name":"protocol1","handler":"protocol1","protocol_segments":[{"id":"ms1","name":"body"}]},"input_characteristic":"example_hex"}}
 
 }
+
+func ExampleWorkerOnPowerCommand() {
+	util.TimeNow = func() time.Time {
+		return time.Time{}
+	}
+	config, err := util.LoadConfig("../../config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config.CompletionStrategy = util.OPTIMISTIC
+	config.CamundaWorkerTimeout = 100
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go lib.Worker(ctx, config, mock.Kafka, mock.Repo, mock.Camunda)
+
+	time.Sleep(1 * time.Second)
+
+	//populate repository
+	mock.Repo.RegisterDevice(model.Device{
+		Id:           "device_1",
+		Name:         "d1",
+		DeviceTypeId: "dt1",
+		LocalId:      "d1u",
+	})
+
+	mock.Repo.RegisterProtocol(model.Protocol{
+		Id:               "p1",
+		Name:             "protocol1",
+		Handler:          "protocol1",
+		ProtocolSegments: []model.ProtocolSegment{{Id: "ms1", Name: "body"}},
+	})
+
+	mock.Repo.RegisterService(model.Service{
+		Id:         "service_1",
+		Name:       "on",
+		LocalId:    "power",
+		ProtocolId: "p1",
+		Inputs: []model.Content{
+			{
+				Id: "metrics",
+				ContentVariable: model.ContentVariable{
+					Id:   "metrics",
+					Name: "metrics",
+					Type: model.Structure,
+					SubContentVariables: []model.ContentVariable{
+						{
+							Id:    "power",
+							Name:  "power",
+							Type:  model.Boolean,
+							Value: true,
+						},
+					},
+				},
+				Serialization:     "json",
+				ProtocolSegmentId: "ms1",
+			},
+		},
+	})
+
+	cmd1 := messages.Command{
+		Function:   model.Function{RdfType: model.SES_ONTOLOGY_CONTROLLING_FUNCTION},
+		DeviceId:   "device_1",
+		ServiceId:  "service_1",
+		ProtocolId: "p1",
+	}
+
+	cmdMsg1, err := json.Marshal(cmd1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mock.Camunda.AddTask(messages.CamundaExternalTask{
+		Id: "1",
+		Variables: map[string]messages.CamundaVariable{
+			util.CAMUNDA_VARIABLES_PAYLOAD: {
+				Value: string(cmdMsg1),
+			},
+		},
+	})
+
+	time.Sleep(1 * time.Second)
+
+	protocolMessageStrings := mock.Kafka.GetProduced("protocol1")
+
+	for _, message := range protocolMessageStrings {
+		fmt.Println(message)
+	}
+
+	//output:
+	//{"request":{"input":{"body":"{\"power\":true}"}},"response":{"output":null},"task_info":{"worker_id":"workerid","task_id":"1","process_instance_id":"","process_definition_id":"","completion_strategy":"optimistic","time":"-62135596800"},"metadata":{"device":{"id":"device_1","local_id":"d1u","name":"d1","device_type_id":"dt1"},"service":{"id":"service_1","local_id":"power","name":"on","description":"","aspects":null,"protocol_id":"p1","inputs":[{"id":"metrics","content_variable":{"id":"metrics","name":"metrics","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"power","name":"power","type":"https://schema.org/Boolean","sub_content_variables":null,"characteristic_id":"","value":true,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},"serialization":"json","protocol_segment_id":"ms1"}],"outputs":null,"functions":null,"rdf_type":""},"protocol":{"id":"p1","name":"protocol1","handler":"protocol1","protocol_segments":[{"id":"ms1","name":"body"}]}}}
+}
