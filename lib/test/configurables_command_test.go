2ac843333f52ed18badd56ea11b80d9e7a3838da
Ingo <roessner@infai.org>
2020-01-23 10:38:19 +0100

    add tests for configurables

--- lib/test/configurables_command_test.go
+++ lib/test/configurables_command_test.go
@@ -431,3 +431,284 @@ func ExampleConfigurablesCommand3() {
 	//{"request":{"input":{"body":"{\"color\":{\"blue\":170,\"green\":0,\"red\":255},\"duration\":3}"}},"response":{"output":null},"task_info":{"worker_id":"workerid","task_id":"3","process_instance_id":"","process_definition_id":"","completion_strategy":"optimistic","time":"-62135596800","tenant_id":"user"},"metadata":{"device":{"id":"device_1","local_id":"d1u","name":"d1","device_type_id":"dt1"},"service":{"id":"service_1","local_id":"s1u","name":"s1","description":"","aspects":null,"protocol_id":"p1","inputs":[{"id":"metrics","content_variable":{"id":"metrics","name":"metrics","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"color","name":"color","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"sr","name":"red","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.r","value":null,"serialization_options":null},{"id":"sg","name":"green","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.g","value":null,"serialization_options":null},{"id":"sb","name":"blue","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.b","value":null,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},{"id":"duration","name":"duration","type":"https://schema.org/Float","sub_content_variables":null,"characteristic_id":"urn:infai:ses:characteristic:9e1024da-3b60-4531-9f29-464addccb13c","value":null,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},"serialization":"json","protocol_segment_id":"ms1"}],"outputs":null,"functions":null,"rdf_type":""},"protocol":{"id":"p1","name":"protocol1","handler":"protocol1","protocol_segments":[{"id":"ms1","name":"body"}]},"input_characteristic":"urn:infai:ses:characteristic:9e1024da-3b60-4531-9f29-464addccb13c"}}
 
 }
+
+func ExampleConfigurablesCommand4() {
+	util.TimeNow = func() time.Time {
+		return time.Time{}
+	}
+	config, err := util.LoadConfig("../../config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config.CompletionStrategy = util.OPTIMISTIC
+	config.CamundaWorkerTimeout = 100
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go lib.Worker(ctx, config, mock.Kafka, mock.Repo, mock.Camunda, mock.Marshaller)
+
+	time.Sleep(1 * time.Second)
+
+	//populate repository
+	mock.Repo.RegisterDevice(model.Device{
+		Id:           "device_1",
+		Name:         "d1",
+		DeviceTypeId: "dt1",
+		LocalId:      "d1u",
+	})
+
+	mock.Repo.RegisterProtocol(model.Protocol{
+		Id:               "p1",
+		Name:             "protocol1",
+		Handler:          "protocol1",
+		ProtocolSegments: []model.ProtocolSegment{{Id: "ms1", Name: "body"}},
+	})
+
+	mock.Repo.RegisterService(model.Service{
+		Id:         "service_1",
+		Name:       "s1",
+		LocalId:    "s1u",
+		ProtocolId: "p1",
+		Inputs: []model.Content{
+			{
+				Id: "metrics",
+				ContentVariable: model.ContentVariable{
+					Id:   "metrics",
+					Name: "metrics",
+					Type: model.Structure,
+					SubContentVariables: []model.ContentVariable{
+						{
+							Id:   "color",
+							Name: "color",
+							Type: model.Structure,
+							SubContentVariables: []model.ContentVariable{
+								{
+									Id:               "sr",
+									Name:             "red",
+									Type:             model.Integer,
+									CharacteristicId: example.Rgb + ".r",
+								},
+								{
+									Id:               "sg",
+									Name:             "green",
+									Type:             model.Integer,
+									CharacteristicId: example.Rgb + ".g",
+								},
+								{
+									Id:               "sb",
+									Name:             "blue",
+									Type:             model.Integer,
+									CharacteristicId: example.Rgb + ".b",
+								},
+							},
+						},
+						{
+							Id:               "duration",
+							Name:             "duration",
+							Type:             model.Float,
+							CharacteristicId: duration.Seconds,
+						},
+					},
+				},
+				Serialization:     "json",
+				ProtocolSegmentId: "ms1",
+			},
+		},
+	})
+
+	cmd := messages.Command{
+		Function:         model.Function{RdfType: model.SES_ONTOLOGY_CONTROLLING_FUNCTION},
+		CharacteristicId: duration.Seconds,
+		DeviceId:         "device_1",
+		ServiceId:        "service_1",
+		ProtocolId:       "p1",
+		Input:            3,
+		Configurables: []marshaller.Configurable{
+			{
+				CharacteristicId: temperature.Celcius,
+				Values: []marshaller.ConfigurableCharacteristicValue{
+					{
+						Value: 255,
+					},
+				},
+			},
+		},
+	}
+
+	cmdMsg, err := json.Marshal(cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mock.Camunda.AddTask(messages.CamundaExternalTask{
+		Id:       "3",
+		TenantId: "user",
+		Variables: map[string]messages.CamundaVariable{
+			util.CAMUNDA_VARIABLES_PAYLOAD: {
+				Value: string(cmdMsg),
+			},
+		},
+	})
+
+	time.Sleep(1 * time.Second)
+
+	protocolMessageStrings := mock.Kafka.GetProduced("protocol1")
+
+	for _, message := range protocolMessageStrings {
+		fmt.Println(message)
+	}
+
+	//output:
+	//{"request":{"input":{"body":"{\"color\":{\"blue\":0,\"green\":0,\"red\":0},\"duration\":3}"}},"response":{"output":null},"task_info":{"worker_id":"workerid","task_id":"3","process_instance_id":"","process_definition_id":"","completion_strategy":"optimistic","time":"-62135596800","tenant_id":"user"},"metadata":{"device":{"id":"device_1","local_id":"d1u","name":"d1","device_type_id":"dt1"},"service":{"id":"service_1","local_id":"s1u","name":"s1","description":"","aspects":null,"protocol_id":"p1","inputs":[{"id":"metrics","content_variable":{"id":"metrics","name":"metrics","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"color","name":"color","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"sr","name":"red","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.r","value":null,"serialization_options":null},{"id":"sg","name":"green","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.g","value":null,"serialization_options":null},{"id":"sb","name":"blue","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.b","value":null,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},{"id":"duration","name":"duration","type":"https://schema.org/Float","sub_content_variables":null,"characteristic_id":"urn:infai:ses:characteristic:9e1024da-3b60-4531-9f29-464addccb13c","value":null,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},"serialization":"json","protocol_segment_id":"ms1"}],"outputs":null,"functions":null,"rdf_type":""},"protocol":{"id":"p1","name":"protocol1","handler":"protocol1","protocol_segments":[{"id":"ms1","name":"body"}]},"input_characteristic":"urn:infai:ses:characteristic:9e1024da-3b60-4531-9f29-464addccb13c"}}
+
+}
+
+func ExampleConfigurablesCommand5() {
+	util.TimeNow = func() time.Time {
+		return time.Time{}
+	}
+	config, err := util.LoadConfig("../../config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config.CompletionStrategy = util.OPTIMISTIC
+	config.CamundaWorkerTimeout = 100
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go lib.Worker(ctx, config, mock.Kafka, mock.Repo, mock.Camunda, mock.Marshaller)
+
+	time.Sleep(1 * time.Second)
+
+	//populate repository
+	mock.Repo.RegisterDevice(model.Device{
+		Id:           "device_1",
+		Name:         "d1",
+		DeviceTypeId: "dt1",
+		LocalId:      "d1u",
+	})
+
+	mock.Repo.RegisterProtocol(model.Protocol{
+		Id:               "p1",
+		Name:             "protocol1",
+		Handler:          "protocol1",
+		ProtocolSegments: []model.ProtocolSegment{{Id: "ms1", Name: "body"}},
+	})
+
+	mock.Repo.RegisterService(model.Service{
+		Id:         "service_1",
+		Name:       "s1",
+		LocalId:    "s1u",
+		ProtocolId: "p1",
+		Inputs: []model.Content{
+			{
+				Id: "metrics",
+				ContentVariable: model.ContentVariable{
+					Id:   "metrics",
+					Name: "metrics",
+					Type: model.Structure,
+					SubContentVariables: []model.ContentVariable{
+						{
+							Id:   "color",
+							Name: "color",
+							Type: model.Structure,
+							SubContentVariables: []model.ContentVariable{
+								{
+									Id:               "sr",
+									Name:             "red",
+									Type:             model.Integer,
+									CharacteristicId: example.Rgb + ".r",
+								},
+								{
+									Id:               "sg",
+									Name:             "green",
+									Type:             model.Integer,
+									CharacteristicId: example.Rgb + ".g",
+								},
+								{
+									Id:               "sb",
+									Name:             "blue",
+									Type:             model.Integer,
+									CharacteristicId: example.Rgb + ".b",
+								},
+							},
+						},
+						{
+							Id:               "duration",
+							Name:             "duration",
+							Type:             model.Float,
+							CharacteristicId: duration.Seconds,
+						},
+					},
+				},
+				Serialization:     "json",
+				ProtocolSegmentId: "ms1",
+			},
+		},
+	})
+
+	cmd := messages.Command{
+		Function:         model.Function{RdfType: model.SES_ONTOLOGY_CONTROLLING_FUNCTION},
+		CharacteristicId: duration.Seconds,
+		DeviceId:         "device_1",
+		ServiceId:        "service_1",
+		ProtocolId:       "p1",
+		Input:            3,
+		Configurables: []marshaller.Configurable{
+			{
+				CharacteristicId: temperature.Celcius,
+				Values: []marshaller.ConfigurableCharacteristicValue{
+					{
+						Value: 255,
+					},
+				},
+			},
+			{
+				CharacteristicId: example.Rgb,
+				Values: []marshaller.ConfigurableCharacteristicValue{
+					{
+						Path:  "r",
+						Value: 255,
+					},
+					{
+						Path:  "g",
+						Value: 0,
+					},
+					{
+						Path:  "b",
+						Value: 170,
+					},
+				},
+			},
+		},
+	}
+
+	cmdMsg, err := json.Marshal(cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mock.Camunda.AddTask(messages.CamundaExternalTask{
+		Id:       "3",
+		TenantId: "user",
+		Variables: map[string]messages.CamundaVariable{
+			util.CAMUNDA_VARIABLES_PAYLOAD: {
+				Value: string(cmdMsg),
+			},
+		},
+	})
+
+	time.Sleep(1 * time.Second)
+
+	protocolMessageStrings := mock.Kafka.GetProduced("protocol1")
+
+	for _, message := range protocolMessageStrings {
+		fmt.Println(message)
+	}
+
+	//output:
+	//{"request":{"input":{"body":"{\"color\":{\"blue\":170,\"green\":0,\"red\":255},\"duration\":3}"}},"response":{"output":null},"task_info":{"worker_id":"workerid","task_id":"3","process_instance_id":"","process_definition_id":"","completion_strategy":"optimistic","time":"-62135596800","tenant_id":"user"},"metadata":{"device":{"id":"device_1","local_id":"d1u","name":"d1","device_type_id":"dt1"},"service":{"id":"service_1","local_id":"s1u","name":"s1","description":"","aspects":null,"protocol_id":"p1","inputs":[{"id":"metrics","content_variable":{"id":"metrics","name":"metrics","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"color","name":"color","type":"https://schema.org/StructuredValue","sub_content_variables":[{"id":"sr","name":"red","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.r","value":null,"serialization_options":null},{"id":"sg","name":"green","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.g","value":null,"serialization_options":null},{"id":"sb","name":"blue","type":"https://schema.org/Integer","sub_content_variables":null,"characteristic_id":"example_rgb.b","value":null,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},{"id":"duration","name":"duration","type":"https://schema.org/Float","sub_content_variables":null,"characteristic_id":"urn:infai:ses:characteristic:9e1024da-3b60-4531-9f29-464addccb13c","value":null,"serialization_options":null}],"characteristic_id":"","value":null,"serialization_options":null},"serialization":"json","protocol_segment_id":"ms1"}],"outputs":null,"functions":null,"rdf_type":""},"protocol":{"id":"p1","name":"protocol1","handler":"protocol1","protocol_segments":[{"id":"ms1","name":"body"}]},"input_characteristic":"urn:infai:ses:characteristic:9e1024da-3b60-4531-9f29-464addccb13c"}}
+
+}
