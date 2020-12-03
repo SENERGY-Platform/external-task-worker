0f1fcec045c359b37701d323d57c5f2e0f7562f4
Ingo <roessner@infai.org>
2020-12-03 11:16:37 +0100

    update device-group model

--- lib/devicerepository/model/shared.go
+++ lib/devicerepository/model/shared.go
@@ -67,16 +67,28 @@ type ProtocolSegment struct {
 }
 
 type DeviceGroup struct {
-	Id                 string           `json:"id"`
-	Name               string           `json:"name"`
-	Image              string           `json:"image"`
-	BlockedInteraction Interaction      `json:"blocked_interaction"` //service must not use this interaction
-	Criteria           []FilterCriteria `json:"criteria"`
-	DeviceIds          []string         `json:"device_ids"`
+	Id            string                      `json:"id"`
+	Name          string                      `json:"name"`
+	Image         string                      `json:"image"`
+	Criteria      []DeviceGroupFilterCriteria `json:"criteria"`
+	DeviceIds     []string                    `json:"device_ids"`
+	CriteriaShort []string                    `json:"criteria_short,omitempty"`
 }
 
-type FilterCriteria struct {
-	FunctionId    string `json:"function_id"`
-	AspectId      string `json:"aspect_id"`
-	DeviceClassId string `json:"device_class_id"`
+func (this *DeviceGroup) SetShortCriteria() {
+	this.CriteriaShort = []string{}
+	for _, criteria := range this.Criteria {
+		this.CriteriaShort = append(this.CriteriaShort, criteria.Short())
+	}
+}
+
+type DeviceGroupFilterCriteria struct {
+	Interaction   Interaction `json:"interaction"`
+	FunctionId    string      `json:"function_id"`
+	AspectId      string      `json:"aspect_id"`
+	DeviceClassId string      `json:"device_class_id"`
+}
+
+func (this DeviceGroupFilterCriteria) Short() string {
+	return this.FunctionId + "_" + this.AspectId + "_" + this.DeviceClassId + "_" + string(this.Interaction)
 }
